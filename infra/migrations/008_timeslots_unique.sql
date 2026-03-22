a1be98f5adc1592348436bee54fafdc2a359db8b
aryan12singh <emailaryan2003@gmail.com>
2026-03-23 01:32:30 +0800

    remove sql

--- infra/migrations/008_timeslots_unique.sql
+++
@@ -1,3 +0,0 @@
--- Add unique constraint on (doctor_id, start_time) to support slot generation with ON CONFLICT DO NOTHING.
-ALTER TABLE doctors.time_slots
-    ADD CONSTRAINT uq_time_slots_doctor_start UNIQUE (doctor_id, start_time);
